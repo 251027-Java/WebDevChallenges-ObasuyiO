3411f3df4a241fc45c7c78a0360196aa910a97de
“Osi” <oobasuyi@uncc.edu>
2025-11-20 10:45:49 -0600

    task 14

--- JSchallenges/Part2/script.js
+++ JSchallenges/Part2/script.js
@@ -1,12 +1,14 @@
 console.log("Hello from the external script!");
 
-function T12() {
-  alert("this is my button for task 12");
-}
 
-// Connect the function to the button
-let btn = document.getElementById("Task12");
-btn.addEventListener("click", T12);
+const list = document.getElementById("myList");
+const button = document.getElementById("addBtn");
+
+button.addEventListener("click", () => {
+    const newItem = document.createElement("li");
+    newItem.textContent = "New Item";
+    list.appendChild(newItem);
+});
 
 
 
